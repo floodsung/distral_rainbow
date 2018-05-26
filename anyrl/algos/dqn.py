52ae0b4e056b0a2d403a7cf0e11b4de7d87c7f00
floodsung <floodsung@gmail.com>
2018-05-26 09:33:56 +0800

    first commit

---
+++ anyrl/algos/dqn.py
@@ -0,0 +1,100 @@
+"""
+The core implementation of deep Q-learning.
+"""
+
+import time
+
+import tensorflow as tf
+import os
+import numpy as np
+import ray
+
+# pylint: disable=R0902,R0903
+
+class DQN:
+    """
+    Train TFQNetwork models using Q-learning.
+    """
+    def __init__(self, online_net, target_net, distill_net, discount=0.99):
+        """
+        Create a Q-learning session.
+
+        Args:
+          online_net: the online TFQNetwork.
+          target_net: the target TFQNetwork.
+          discount: the per-step discount factor.
+        """
+        self.online_net = online_net
+        self.target_net = target_net
+        self.distill_net = distill_net
+        self.discount = discount
+
+        obs_shape = (None,) + online_net.obs_vectorizer.out_shape
+        self.obses_ph = tf.placeholder(online_net.input_dtype, shape=obs_shape)
+        self.actions_ph = tf.placeholder(tf.int32, shape=(None,))
+        self.rews_ph = tf.placeholder(tf.float32, shape=(None,))
+        self.new_obses_ph = tf.placeholder(online_net.input_dtype, shape=obs_shape)
+        self.terminals_ph = tf.placeholder(tf.bool, shape=(None,))
+        self.discounts_ph = tf.placeholder(tf.float32, shape=(None,))
+        self.weights_ph = tf.placeholder(tf.float32, shape=(None,))
+
+        self.log_distill_policy = self.distill_net.log_policy(self.new_obses_ph)
+        losses,self.distill_loss = online_net.transition_loss(target_net,self.log_distill_policy, self.obses_ph, self.actions_ph,
+                                            self.rews_ph, self.new_obses_ph, self.terminals_ph,
+                                            self.discounts_ph)
+        self.losses = self.weights_ph * losses
+        self.loss = tf.reduce_mean(self.losses)
+
+        self.distill_variables = ray.experimental.TensorFlowVariables(self.log_distill_policy, self.online_net.session)
+
+        assigns = []
+        for dst, src in zip(target_net.variables, online_net.variables):
+            assigns.append(tf.assign(dst, src))
+        self.update_target = tf.group(*assigns)
+
+        self.optim = tf.train.AdamOptimizer(learning_rate=1e-4, epsilon=1.5e-4).minimize(self.loss)
+        distill_optim = tf.train.AdamOptimizer(learning_rate=1e-4, epsilon=1.5e-4)
+        self.distill_grads = distill_optim.compute_gradients(self.distill_loss)
+        self.train_distill_policy = distill_optim.apply_gradients(self.distill_grads)
+
+    def feed_dict(self, transitions):
+        """
+        Generate a feed_dict that feeds the batch of
+        transitions to the DQN loss terms.
+
+        Args:
+          transition: a sequence of transition dicts, as
+            defined in anyrl.rollouts.ReplayBuffer.
+
+        Returns:
+          A dict which can be fed to tf.Session.run().
+        """
+        obs_vect = self.online_net.obs_vectorizer
+        res = {
+            self.obses_ph: obs_vect.to_vecs([t['obs'] for t in transitions]),
+            self.actions_ph: [t['model_outs']['actions'][0] for t in transitions],
+            self.rews_ph: [self._discounted_rewards(t['rewards']) for t in transitions],
+            self.terminals_ph: [t['new_obs'] is None for t in transitions],
+            self.discounts_ph: [(self.discount ** len(t['rewards'])) for t in transitions],
+            self.weights_ph: [t['weight'] for t in transitions]
+        }
+        new_obses = []
+        for trans in transitions:
+            if trans['new_obs'] is None:
+                new_obses.append(trans['obs'])
+            else:
+                new_obses.append(trans['new_obs'])
+        res[self.new_obses_ph] = obs_vect.to_vecs(new_obses)
+        return res
+
+    def get_distill_policy_weights(self):
+        return self.distill_variables.get_weights()
+
+    def set_distill_policy_weights(self,weights):
+        self.distill_variables.set_weights(weights)
+
+    def _discounted_rewards(self, rews):
+        res = 0
+        for i, rew in enumerate(rews):
+            res += rew * (self.discount ** i)
+        return res
---
+++ distral_agent.py
@@ -0,0 +1,163 @@
+import tensorflow as tf
+
+from anyrl.algos import DQN
+from anyrl.envs import BatchedGymEnv
+from anyrl.envs.wrappers import BatchedFrameStack
+from anyrl.models import rainbow_models
+from anyrl.rollouts import BatchedPlayer, PrioritizedReplayBuffer, NStepPlayer
+from anyrl.spaces import gym_space_vectorizer
+
+from sonic_util import AllowBacktracking, make_env
+import numpy as np
+import csv
+import ray
+import time
+
+THREAD_NUM = 32
+NUM_ITER  = 5000000
+
+class DistralAgent():
+
+    def __init__(self,env_index):
+        # step 1: init env
+        self.env_index = env_index
+        self.env = self.init_env(env_index)
+
+        # step 2: init rainbow
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
+
+        self.dqn = DQN(*rainbow_models(self.sess,
+                                  self.env.action_space.n,
+                                  gym_space_vectorizer(self.env.observation_space),
+                                  min_val=-200,
+                                  max_val=200))
+        self.player = NStepPlayer(BatchedPlayer(self.env, self.dqn.online_net), 3)
+        self.sess.run(tf.global_variables_initializer())
+
+        self.replay_buffer = PrioritizedReplayBuffer(500000, 0.5, 0.4, epsilon=0.1)
+        self.sess.run(self.dqn.update_target)
+        self.steps_taken = 0
+        self.train_interval=1
+        self.target_interval=8192
+        self.batch_size=32
+        self.min_buffer_size=200
+        self.handle_ep=lambda steps, rew: None
+        self.next_target_update = self.target_interval
+        self.next_train_step = self.train_interval
+
+
+    def init_env(self,env_index):
+        train_file = csv.reader(open('./sonic-train.csv','r'),delimiter=',')
+        self.games = []
+        for i,row in enumerate(train_file):
+            if i == 0:
+                continue
+            self.games.append(row)
+
+        env = AllowBacktracking(make_env(game=self.games[env_index][0],state=self.games[env_index][1]))
+
+        env = BatchedFrameStack(BatchedGymEnv([[env]]), num_images=4, concat=False)
+        return env
+
+    def train(self,distill_policy_weights):
+
+        self.dqn.set_distill_policy_weights(distill_policy_weights)
+
+        transitions = self.player.play()
+        distill_grads = 0
+        for trans in transitions:
+                if trans['is_last']:
+                    self.handle_ep(trans['episode_step'] + 1, trans['total_reward'])
+                self.replay_buffer.add_sample(trans)
+                self.steps_taken += 1
+                if self.replay_buffer.size >= self.min_buffer_size and self.steps_taken >= self.next_train_step:
+                    self.next_train_step = self.steps_taken + self.train_interval
+                    batch = self.replay_buffer.sample(self.batch_size)
+
+                    grad_names = []
+                    for grad in self.dqn.distill_grads:
+                        if grad[0] != None:
+                            grad_names.append(grad[0])
+
+                    _,losses,distill_grads = self.sess.run((self.dqn.optim,self.dqn.losses,grad_names),
+                                         feed_dict=self.dqn.feed_dict(batch))
+                    self.replay_buffer.update_weights(batch, losses)
+
+                if self.steps_taken >= self.next_target_update:
+                    self.next_target_update = self.steps_taken + self.target_interval
+                    self.sess.run(self.dqn.update_target)
+
+        return distill_grads
+
+
+def main():
+
+    ray.init()
+
+    remote_agent = ray.remote(DistralAgent)
+    agents = [remote_agent.remote(i) for i in range(THREAD_NUM)]
+
+    #joint agent
+    env = AllowBacktracking(make_env())
+    env = BatchedFrameStack(BatchedGymEnv([[env]]), num_images=4, concat=False)
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True # pylint: disable=E1101
+    sess = tf.Session(config=config)
+    local_dqn = DQN(*rainbow_models(sess,
+                                  env.action_space.n,
+                                  gym_space_vectorizer(env.observation_space),
+                                  min_val=-200,
+                                  max_val=200))
+    sess.run(tf.global_variables_initializer())
+
+    distill_network_variables = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope="distill")
+    saver = tf.train.Saver(distill_network_variables)
+    # checkpoint = tf.train.get_checkpoint_state("./models")
+    # if checkpoint and checkpoint.model_checkpoint_path:
+    #     saver.restore(sess, checkpoint.model_checkpoint_path)
+    #     print ("Successfully loaded:", checkpoint.model_checkpoint_path)
+    # else:
+    #     print ("Could not find old network weights")
+
+    grad_names = []
+    for grad in local_dqn.distill_grads:
+        if grad[0] != None:
+            grad_names.append(grad[0])
+
+    weights = local_dqn.get_distill_policy_weights()
+
+    for iteration in range(NUM_ITER):
+        start = time.time()
+        if iteration % 1000 == 0:
+            print("iter:",iteration)
+
+        weights_id = ray.put(weights)
+
+        gradients_ids = [agent.train.remote(weights_id) for agent in agents]
+        gradients_list = ray.get(gradients_ids)
+
+        if not 0 in gradients_list:
+            mean_grads = [sum([gradients[i] for gradients in gradients_list]) / len(gradients_list) for i in range(len(gradients_list[0]))]
+            feed_dict = {grad: mean_grad for (grad, mean_grad) in zip(grad_names, mean_grads)}
+            sess.run(local_dqn.train_distill_policy, feed_dict=feed_dict)
+
+        weights = local_dqn.get_distill_policy_weights()
+
+        if (iteration+1)% 20000 == 0:
+            if not os.path.exists("./models"):
+                os.makedirs("./models")
+            saver.save(sess, './models/' + 'network', global_step = 10000)
+        if iteration % 10 == 0:
+            print("iter time:",time.time() - start)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
