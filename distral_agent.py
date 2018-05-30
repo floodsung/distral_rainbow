886a561b92c9b46263b03f63307fcff276d944ed
songrotek <songrotek@qq.com>
2018-05-30 14:57:44 +0800

    update

--- distral_agent.py
+++ distral_agent.py
@@ -14,7 +14,6 @@ import ray
 import time
 import os
 import pdb
-np.random.seed(0)
 
 THREAD_NUM = 8
 NUM_ITER  = 5000000
@@ -24,7 +23,6 @@ AGENT_NUM_PER_THREAD = 6
 class MultiAgent():
     """docstring for MultiAgent"""
     def __init__(self,thread_index,num_agent=AGENT_NUM_PER_THREAD):
-        np.random.seed(0)
         os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(map(str, ray.get_gpu_ids()))
         config = tf.ConfigProto()
         config.gpu_options.allow_growth = True
@@ -54,7 +52,6 @@ class MultiAgent():
 class SonicEnv():
 
     def __init__(self,env_index):
-        np.random.seed(0)
         train_file = csv.reader(open('./sonic-train.csv','r'),delimiter=',')
         self.games = []
         for i,row in enumerate(train_file):
