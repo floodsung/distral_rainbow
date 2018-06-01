6bd904d8ac38774adfcf8968af6f2dfd7e54b1db
songrotek <songrotek@qq.com>
2018-06-01 13:26:30 +0800

    update

--- anyrl/models/dqn_dist.py
+++ anyrl/models/dqn_dist.py
@@ -138,13 +138,13 @@ class DistQNetwork(TFQNetwork):
         policy, values, dists = self.session.run(self.step_outs, feed_dict=feed)
         isnan = any(np.isnan(p) for p in policy[0])
         if not isnan:
-            actions = np.argmax(policy[0]),
+            actions = np.argmax(policy,axis=1)
         else:
             pdb.set_trace()
-            actions = 4
+            actions = [4]
 
         return {
-            'actions': [actions],
+            'actions': actions,
             'states': None,
             'policy':policy,
             'action_values': values,
